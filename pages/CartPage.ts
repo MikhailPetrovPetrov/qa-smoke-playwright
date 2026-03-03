52ad713e58f891c5ade814bb4124dd7ae25134e2
Mikhail Petrov <petrovmikle69@gmail.com>
2026-03-03 14:59:51 +0100

    Add product psge test and cart page test

--- pages/CartPage.ts
+++
@@ -1,9 +0,0 @@
-import { Page, expect } from '@playwright/test';
-
-export class CartPage {
-  constructor(private page: Page) {}
-
-  async verifyProductInCart() {
-    await expect(this.page.locator('.cart_item')).toBeVisible();
-  }
-}
