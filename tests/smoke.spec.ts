285692e37def64271979f57f116625f064725405
Mikhail Petrov <petrovmikle69@gmail.com>
2026-03-03 12:03:04 +0100

    Add product psge test and cart page test

--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,10 +1,21 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { ProductsPage } from '../pages/ProductsPage';
+import { CartPage } from '../pages/CartPage';
 
 
 test('Smoke: user can add product to cart', async ({ page }) => {
   const login = new LoginPage(page);
+  const products = new ProductsPage(page);
+  const cart = new CartPage(page);
+
   await login.open();
   await login.login('standard_user', 'secret_sauce');
   await login.shouldBeLoggedIn();
+
+  await products.verifyLoaded();
+  await products.addFirstProduct();
+  await products.openCart();
+
+  await cart.verifyProductInCart();
 });
